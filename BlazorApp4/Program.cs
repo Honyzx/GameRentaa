a07393ef34c9d47425c6455d5080d76083531366
admin <sofav4940@gmail.com>
2025-06-22 12:39:28 +0400

    бд

--- BlazorApp4/Program.cs
+++ BlazorApp4/Program.cs
@@ -34,14 +34,14 @@ using (var serviceScope = app.Services.CreateScope())
     if (!db.Games.Any())
     {
         var video = new Category {Name = "Видео игры"};
-        var board = new Category {Name = "Настольные игры"};
+        var board = new Category {Name = "Настольные игры"}; 
         db.Games.AddRange(
-            new Game { Title = "Horizon Zero Dawn Remastered PS5", Description = "image/Elden.jpg", DailyRentalPrice = 499 , Category = video  },
-            new Game { Title  = "ELDEN RING NIGHTREIGN PS4 & PS5", Description = "https://igroarenda.ru/image/cache/catalog/hzz-500x500.jpg", DailyRentalPrice = 899 , Category = video},
-            new Game { Title  = "Alien: Isolation", Description = "https://igroarenda.ru/image/cache/catalog/alien-500x500.jpg", DailyRentalPrice = 99 , Category = video },
-            new Game { Title  = "Forza Horizon 5 Standard Edition PS5", Description = "https://igroarenda.ru/image/cache/catalog/reviews/forzz-500x500.jpg", DailyRentalPrice = 899, Category = video },
-            new Game { Title  = "Бункер настольная игра", Description = "https://www.tdkarandash.ru/upload/iblock/9f2/74mdg4b5gxprb9b72qbz6in3f44tw2uj.jpg", DailyRentalPrice = 199 ,Category = board },
-            new Game { Title  = "Детективные истории: Последний рейс Гаттардо", Description = "https://www.tdkarandash.ru/upload/resize_cache/iblock/17b/450_450_140cd750bba9870f18aada2478b24840a/p9y01c8zalmpjhthz1ntxk147flw0f7g.jpg", DailyRentalPrice = 399 , Category = board }
+            new Game { Title = "Horizon Zero Dawn Remastered PS5", Description = "499", DailyRentalPrice = 499 , Category = video, GameID = 1,  },
+            new Game { Title  = "ELDEN RING NIGHTREIGN PS4 & PS5", Description = "https://igroarenda.ru/image/cache/catalog/hzz-500x500.jpg", DailyRentalPrice = 899 , Category = video, GameID = 2,},
+            new Game { Title  = "Alien: Isolation", Description = "https://igroarenda.ru/image/cache/catalog/alien-500x500.jpg", DailyRentalPrice = 99 , Category = video , GameID = 3,},
+            new Game { Title  = "Forza Horizon 5 Standard Edition PS5", Description = "https://igroarenda.ru/image/cache/catalog/reviews/forzz-500x500.jpg", DailyRentalPrice = 899, Category = video, GameID = 4, },
+            new Game { Title  = "Бункер настольная игра", Description = "https://www.tdkarandash.ru/upload/iblock/9f2/74mdg4b5gxprb9b72qbz6in3f44tw2uj.jpg", DailyRentalPrice = 199 ,Category = board, GameID = 5, },
+            new Game { Title  = "Детективные истории: Последний рейс Гаттардо", Description = "https://www.tdkarandash.ru/upload/resize_cache/iblock/17b/450_450_140cd750bba9870f18aada2478b24840a/p9y01c8zalmpjhthz1ntxk147flw0f7g.jpg", DailyRentalPrice = 399 , Category = board, GameID = 6, }
         );
         db.SaveChanges();
     }
